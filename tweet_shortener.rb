d06ec400166026bdfc43d655075fc1ae621a2952
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-19 12:56:00 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def dictionary 
+  dictionary = {hello:"hi", to:"2", two:"2", too:"2", :for => "4", four:"4", be: "b", you:"u", at:"@", :and=> "&"}
+  
+end
+
+
+def word_substituter(tweet)
+  new_tweet = tweet.split
+  dict = dictionary
+  new_tweet.map!{ |word|
+   if dict.include?word
+    word=dict[word]
+  else word = word
+  end
+  }
+
+  
+end
