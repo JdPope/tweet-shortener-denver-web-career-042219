69971044ae97fd9244ded84cdca1ef097960897d
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-19 13:25:26 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -20,5 +20,16 @@ return new_array.join(" ")
 end
 
 
-def bulk_tweet_shortener
+def bulk_tweet_shortener(tweet)
+  new_tweet = tweet.split
+  dict = dictionary
+  dict_keys = dict.keys
+ new_array= new_tweet.map { |word|
+   if dict_keys.include?word
+    dict[word]
+  else word
+  end
+  }
+puts new_array.join(" ")
+  
 end
